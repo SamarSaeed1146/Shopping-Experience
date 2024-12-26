4f8d2b56fb8d3955932a863883cd04fe8e66cb52
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-12-26 19:30:11 +0500

    Add image tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,16 @@ import posterImg from "@/public/assets/posters.png";
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <div className="flex flex-col md:flex-row items-center bg-primary md:h-96">
+      <div className="flex items-center bg-primary md:h-96">
         <div className="space-y-7 p-10 text-center md:w-1/2">
           <h1 className="text-3xl font-bold">Welcome to Our Store</h1>
           <p className="text-lg">Discover amazing products and great deals!</p>
         </div>
-        <div className="h-full w-full md:w-1/2 relative">
+        <div className="hidden h-full md:block md:w-1/2">
           <Image
             src={posterImg}
             alt="Shopping Experience Poster"
-            fill
-            style={{ objectFit: 'cover' }}
-            priority
+            className="h-full object-cover"
           />
         </div>
       </div>
