55a48b7fb770008280a9ca2bc2436eddb989a27f
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-12-30 21:32:24 +0500

    Backgroung Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,77 @@
 import Image from "next/image";
 import posterImg from "@/public/assets/posters.png";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { delay } from "../lib/utils";
+import { Suspense } from "react";
+import { getWixClient } from "../lib/wix-client.base";
+import Product from "../components/Product";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <div className="flex items-center bg-primary md:h-96">
+      <div className="flex items-center bg-secondary md:h-96">
         <div className="space-y-7 p-10 text-center md:w-1/2">
-          <h1 className="text-3xl font-bold">Welcome to Our Store</h1>
-          <p className="text-lg">Discover amazing products and great deals!</p>
+          <h1 className="text-3xl font-bold md:text-4xl">
+            Fill the void in your heart
+          </h1>
+          <p className="text-lg">
+            Tough day? Credit card maxed out? Buy some expensive stuff and
+            become happy again
+          </p>
+          <Button asChild>
+            <Link href="/shop">
+              Shop Now <ArrowRight className="ml-2 size-5" />
+            </Link>
+          </Button>
         </div>
-        <div className="hidden h-full md:block md:w-1/2">
+        <div className="relative hidden h-full md:block md:w-1/2">
           <Image
             src={posterImg}
             alt="Shopping Experience Poster"
             className="h-full object-cover"
           />
+          <div className="absolute inset-0 bg-gradient-to-r from-secondary via-transparent to-transparent" />
         </div>
       </div>
+      <Suspense fallback={"Loading..."}>
+        <FeaturedProducts />
+      </Suspense>
     </main>
+  );
+}
+
+async function FeaturedProducts() {
+  await delay(1000);
+
+  const wixClient = getWixClient();
+
+  const { collection } =
+    await wixClient.collections.getCollectionBySlug("featured-products");
+
+  if (!collection?._id) {
+    return null;
+  }
+
+  const featuredProducts = await wixClient.products
+    .queryProducts()
+    .hasSome("collectionIds", [collection._id])
+    .descending("lastUpdated")
+    .find();
+
+  if (!featuredProducts.items.length) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Featured Products</h2>
+      <div className="flex gap-5 grid-cols-2 flex-col sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {featuredProducts.items.map((product => (
+          <Product key={product._id} product={product} />
+        )))}
+      </div>
+    </div>
   );
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import { products } from "@wix/stores";
+import Link from "next/link";
+import { media as wixMedia } from "@wix/sdk";
+
+interface ProductProps {
+  product: products.Product;
+}
+
+export default function Product({ product }: ProductProps) {
+  const mainImage = product.media?.mainMedia?.image;
+
+  const resizedImageUrl = mainImage?.url
+    ? wixMedia.getScaledToFillImageUrl(mainImage.url, 700, 700, {})
+    : null;
+
+  return (
+    <Link href={`/product/${product.slug}`} className="h-full border">
+      <div className="overflow-hidden">
+        <img
+          src={resizedImageUrl || "/placeholder.png"}
+          alt={mainImage?.altText || ""}
+          className="transform transition-transform duration-300 hover:scale-110"
+        />
+      </div>
+      <div className="space-y-3 p-3">
+        <h3 className="text-lg font-bold">{product.name}</h3>
+        <div
+          className="line-clamp-5"
+          dangerouslySetInnerHTML={{ __html: product.description || "" }}
+        />
+      </div>
+    </Link>
+  );
+}
