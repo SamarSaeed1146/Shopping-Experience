9d7837e32bc851227bee122057195ee5666ae3f2
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-02-04 18:21:02 +0500

    Added Navbar & Cart

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { getWixClient } from "../lib/wix-client.base";
+import logo from "@/public/shopping-logo.png"
+import Image from "next/image";
+
+async function getCart() {
+  const wixClient = getWixClient();
+  try {
+    return await wixClient.currentCart.getCurrentCart();
+  } catch (error) {
+    if (
+      (error as any).details.applicationError.code === "OWNED_CART_NOT_FOUND"
+    ) {
+      return null;
+    } else {
+      throw error;
+    }
+  }
+}
+
+export default async function Navbar() {
+  const cart = await getCart();
+
+  const totalQuantity =
+    cart?.lineItems.reduce((acc, item) => acc + (item.quantity || 0), 0) || 0;
+
+  return (
+    <header className="bg-background shadow-sm">
+      <div className="max-w-7xl mx-auto p-5 flex justify-between items-center gap-5">
+        <Link href="/"className="flex items-center gap-4">
+          <Image src={logo} alt="Shopping Logo" width={40} height={40} />
+          <span className="text-xl font-bold">Shopping Experience</span>
+        </Link>
+        {totalQuantity} items in cart
+      </div>
+    </header>
+  );
+}
