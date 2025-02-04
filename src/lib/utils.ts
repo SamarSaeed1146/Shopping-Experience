523e2555e9f11cd180c2f26f02159887b28f42fb
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-02-04 15:16:11 +0500

    Addded Currency Function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-
 export async function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function formatCurrency(
+  price: number | string = 0,
+  currency: string = "PKR",
+) {
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(Number(price));
 }
