274443831b3087be94208d778eac1130f142dc04
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-02-06 01:18:53 +0500

    Update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,9 @@
 import { products } from "@wix/stores";
 import Link from "next/link";
 import WixImage from "./WixImage";
+import Badge from "./ui/badge";
+import { formatCurrency } from "../lib/utils";
+import DiscountBadge from "./DiscountBadge";
 
 interface ProductProps {
   product: products.Product;
@@ -10,7 +13,7 @@ export default function Product({ product }: ProductProps) {
   const mainImage = product.media?.mainMedia?.image;
 
   return (
-    <Link href={`/product/${product.slug}`} className="h-full border">
+    <Link href={`/product/${product.slug}`} className="h-full border bg-card rounded-sm overflow-hidden shadow-md">
       <div className="relative overflow-hidden">
         <WixImage
           width={700}
@@ -19,8 +22,12 @@ export default function Product({ product }: ProductProps) {
           alt={mainImage?.altText}
           className="transform transition-transform duration-300 hover:scale-110"
         />
-        <div className="absolute">
-          
+        <div className="absolute bottom-3 right-3 flex flex-wrap items-center gap-2"> 
+          {product.ribbon && <Badge>{product.ribbon}</Badge>}
+          {product.discount && <DiscountBadge data={product.discount} />}
+          <Badge
+          className="bg-secondary text-secondary-foreground font-semibold"
+          >{getFormattedPrice(product)}</Badge>
         </div>
       </div>
       <div className="space-y-3 p-3">
@@ -32,4 +39,19 @@ export default function Product({ product }: ProductProps) {
       </div>
     </Link>
   );
+}
+
+function getFormattedPrice(product: products.Product) {
+  const minPrice = product.priceRange?.minValue;
+  const maxPrice = product.priceRange?.minValue;
+
+  if (minPrice && maxPrice && minPrice !== maxPrice) {
+    return `from ${formatCurrency(minPrice, product.priceData?.currency)}`;
+  } else {
+    return (
+      product.priceData?.formatted?.discountedPrice ||
+      product.priceData?.formatted?.price ||
+      "N/A"
+    );
+  }
 }
