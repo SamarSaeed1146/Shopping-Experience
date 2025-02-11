f891d5dde055593f8addadc3a32e4d44ae7e9460
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-02-11 09:03:16 +0500

    Add Function FindVariant

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { products } from "@wix/stores";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -17,4 +18,19 @@ export function formatCurrency(
     style: "currency",
     currency,
   }).format(Number(price));
+}
+
+export function findVariant(
+  product: products.Product,
+  selectedOptions: Record<string, string>,
+) {
+  if (!product.manageVariants) return null;
+
+  return (
+    product.variants?.find((variant) => {
+      return Object.entries(selectedOptions).every(
+        ([key, value]) => variant.choices?.[key] === value,
+      );
+    }) || null
+  );
 }
