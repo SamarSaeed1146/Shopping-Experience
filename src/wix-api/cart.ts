e0879081f57bca84e3a1b1915dda12729f60ccb2
SamarSaeed1146 <samarsaeed374@gmail.com>
2025-02-19 13:18:51 +0500

    Add interface

--- src/wix-api/cart.ts
+++ src/wix-api/cart.ts
@@ -1,44 +1,51 @@
 import { products } from "@wix/stores";
 import { getWixClient } from "../lib/wix-client.base";
 import { findVariant } from "../lib/utils";
+import { WIX_STORES_APP_ID } from "../lib/constants";
 
 export async function getCart() {
-    const wixClient = getWixClient();
-    try {
-      return await wixClient.currentCart.getCurrentCart();
-    } catch (error) {
-      if (
-        (error as any).details.applicationError.code === "OWNED_CART_NOT_FOUND"
-      ) {
-        return null;
-      } else {
-        throw error;
-      }
+  const wixClient = getWixClient();
+  try {
+    return await wixClient.currentCart.getCurrentCart();
+  } catch (error) {
+    if (
+      (error as any).details.applicationError.code === "OWNED_CART_NOT_FOUND"
+    ) {
+      return null;
+    } else {
+      throw error;
     }
   }
-  interface AddToCartValues {
-    product: products.Product,
-    selectedOptions: Record<string,string>,
-    quantity: number
-  }
+}
+interface AddToCartValues {
+  product: products.Product;
+  selectedOptions: Record<string, string>;
+  quantity: number;
+}
 
-  export async function addToCart({
-    product,
-    selectedOptions,
-    quantity
-  }: AddToCartValues) {
-    const wixClient = getWixClient()
+export async function addToCart({
+  product,
+  selectedOptions,
+  quantity,
+}: AddToCartValues) {
+  const wixClient = getWixClient();
 
-    const selectedVariant = findVariant(product, selectedOptions)
+  const selectedVariant = findVariant(product, selectedOptions);
 
-    return wixClient.currentCart.addToCurrentCart({
-      lineItems: [
-        {
-          catalogReference: {
-            appId:
-          }
-        }
-      ]
-    })
-  }
-   
+  return wixClient.currentCart.addToCurrentCart({
+    lineItems: [
+      {
+        catalogReference: {
+          appId: WIX_STORES_APP_ID,
+          catalogItemId: product._id,
+          options: selectedVariant
+            ? {
+                variantId: selectedVariant._id,
+              }
+            : { options: selectedOptions },
+        },
+        quantity,
+      },
+    ],
+  });
+}
